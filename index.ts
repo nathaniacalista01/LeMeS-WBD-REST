371f682d8efccbd20c47d6cededd2fe4ff10361f
nathaniacalista01 <nathania.calista01@gmail.com>
2023-10-25 19:52:22 +0700

    Initial commit

---
+++ index.ts
@@ -0,0 +1,10 @@
+import express, {Request,Response} from "express"
+
+const app = express();
+
+app.get("/",(req : Request, res : Response) =>{
+    res.send("Testing");
+})
+app.listen(3000,()=>{
+    console.log("App is listening");
+})
