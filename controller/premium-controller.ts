22ec871899faa374fa05a416a687f456dea4120a
nathaniacalista01 <nathania.calista01@gmail.com>
2023-11-11 01:00:44 +0700

    feat : update premium status function

--- controller/premium-controller.ts
+++ controller/premium-controller.ts
@@ -4,32 +4,65 @@ import { Premium } from "../types/type";
 export const premiumRouter = express.Router();
 const soap = require("soap");
 
+// Router used to get all upgrade premium request
 premiumRouter.get("/", (req: Request, res: Response) => {
   const soap_url: string | undefined = process.env.SOAP_URL;
   soap.createClient(soap_url, (err: any, client: any) => {
     if (err) {
-      console.log(err);
-      console.log("Error connecting to SOAP...");
-      res.status(500).send("Internal server error");
+      res.json({
+        status: 500,
+        message: "Internal server error",
+      });
     } else {
       client.getAllPremium({}, (err: any, result: any) => {
         if (err) {
           console.error("Error calling getAllPremium", err);
-          res.status(500).send("Internal server error");
+          res.json({
+            status: 500,
+            message: "Internal server error",
+          });
         } else {
           const soapData: any = result["return"];
 
           // Membentuk objek JSON yang diinginkan
-          const formattedData : Premium[] = JSON.parse(soapData).data;
-            res.json({
-                status : 200,
-                message : "Sucesfully get all data",
-                data : formattedData,
-            })
+          const formattedData: Premium[] = JSON.parse(soapData).data;
+          res.json({
+            status: 200,
+            message: "Sucesfully get all data",
+            data: formattedData,
+          });
         }
       });
     }
   });
-  // console.log(soap_url)
-  // soap.createClient()
+});
+
+premiumRouter.put("/:user_id",  (req: Request, res: Response) => {
+    const {user_id} = req.params
+    const {new_status} = req.body;
+    const soap_url : string | undefined = process.env.SOAP_URL;
+    if(soap_url){
+        soap.createClient(soap_url,(err : any,client : any) =>{
+            if(err){
+                return res.json({
+                    status : 500,
+                    message : "Internal server error"
+                })
+            }else{
+                client.updatePremiumStatus({user_id, newStatus : new_status}, (err : any, result : any)=>{
+                    if(err){
+                        return res.json({
+                            status : 500,
+                            message : "Internal server error"
+                        })
+                    }else{
+                        return res.json({
+                            status : 200,
+                            message : result["return"]
+                        })
+                    }
+                } )
+            }
+        })
+    }
 });
--- dist/controller/premium-controller.js
+++ dist/controller/premium-controller.js
@@ -7,19 +7,24 @@ exports.premiumRouter = void 0;
 const express_1 = __importDefault(require("express"));
 exports.premiumRouter = express_1.default.Router();
 const soap = require("soap");
+// Router used to get all upgrade premium request
 exports.premiumRouter.get("/", (req, res) => {
     const soap_url = process.env.SOAP_URL;
     soap.createClient(soap_url, (err, client) => {
         if (err) {
-            console.log(err);
-            console.log("Error connecting to SOAP...");
-            res.status(500).send("Internal server error");
+            res.json({
+                status: 500,
+                message: "Internal server error",
+            });
         }
         else {
             client.getAllPremium({}, (err, result) => {
                 if (err) {
                     console.error("Error calling getAllPremium", err);
-                    res.status(500).send("Internal server error");
+                    res.json({
+                        status: 500,
+                        message: "Internal server error",
+                    });
                 }
                 else {
                     const soapData = result["return"];
@@ -34,6 +39,35 @@ exports.premiumRouter.get("/", (req, res) => {
             });
         }
     });
-    // console.log(soap_url)
-    // soap.createClient()
+});
+exports.premiumRouter.put("/:user_id", (req, res) => {
+    const { user_id } = req.params;
+    const { new_status } = req.body;
+    const soap_url = process.env.SOAP_URL;
+    if (soap_url) {
+        soap.createClient(soap_url, (err, client) => {
+            if (err) {
+                return res.json({
+                    status: 500,
+                    message: "Internal server error"
+                });
+            }
+            else {
+                client.updatePremiumStatus({ user_id, newStatus: new_status }, (err, result) => {
+                    if (err) {
+                        return res.json({
+                            status: 500,
+                            message: "Internal server error"
+                        });
+                    }
+                    else {
+                        return res.json({
+                            status: 200,
+                            message: result["return"]
+                        });
+                    }
+                });
+            }
+        });
+    }
 });
