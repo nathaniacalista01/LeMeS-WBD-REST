a62ae4487903c0befb44bb2faacba942acc02ddb
nathaniacalista01 <nathania.calista01@gmail.com>
2023-11-15 00:34:01 +0700

    fix : cors

--- app.ts
+++ app.ts
@@ -1,20 +1,33 @@
-import express, { Express } from "express"
-import cors from "cors"
+import express, { Express } from "express";
+import cors from "cors";
 import router from "./routes/route";
 
-
 class App {
-    private server: Express;
-    constructor() {
-        this.server = express();
-        this.server.use(cors());
-        this.server.use(express.json());
-        this.server.use("/api", router);
-    }
-    public run() {
-        this.server.listen(8000, () => {
-            console.log("Server is listening...");
-        })
-    }
+  private server: Express;
+  constructor() {
+    const allowedOrigins = process.env.VALID_ORIGIN?.split(",");
+    const corsOptions = {
+      credentials: true,
+      origin: (
+        origin: string | undefined,
+        callback: (error: null | Error, allow?: boolean) => void
+      ) => {
+        if (!origin || allowedOrigins?.includes(origin)) {
+          callback(null, true);
+        } else {
+          callback(new Error("Not allowed by CORS"));
+        }
+      },
+    };
+    this.server = express();
+    this.server.use(cors(corsOptions));
+    this.server.use(express.json());
+    this.server.use("/api", router);
+  }
+  public run() {
+    this.server.listen(8000, () => {
+      console.log("Server is listening...");
+    });
+  }
 }
 export default App;
