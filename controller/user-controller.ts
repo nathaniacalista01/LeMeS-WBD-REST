630ad18c59e6e4854d428d843178facc373b07d4
nathaniacalista01 <nathania.calista01@gmail.com>
2023-11-16 01:54:26 +0700

    feat : endpoint for checking is admin

--- controller/user-controller.ts
+++ controller/user-controller.ts
@@ -3,6 +3,8 @@ import { UserService } from "../service/user-service";
 import { Error, Success } from "../types/type";
 import { adminMiddleware } from "../middleware/admin-middleware";
 import { loginMiddleware } from "../middleware/login-middleware";
+import { FailedResponse, SuccessResponse } from "../utils/template";
+import { Payload } from "../utils/payload";
 
 export const userRouter = express.Router();
 
@@ -33,82 +35,88 @@ userRouter.post("/", async (req: Request, res: Response) => {
     image_path
   );
   if (response === Error.REGISTER_FAILED) {
-    return res.json({
-      status: 400,
-      message: Error.REGISTER_FAILED,
-    });
+    return res.json(new FailedResponse(500, Error.REGISTER_FAILED));
   }
-  return res.json({
-    status: 200,
-    message: response,
-  });
+  return res.json(new SuccessResponse(response));
 });
 
-userRouter.put("/:user_id", loginMiddleware, async (req: Request, res: Response) => {
-  const { username, fullname, password, image_path } = req.body;
-  const { user_id } = req.params;
-  const user_service = new UserService();
-  const response = await user_service.editUser(
-    parseInt(user_id),
-    username,
-    fullname,
-    password,
-    image_path
-  );
-  if (response === Error.EDIT_FAILED) {
+userRouter.put(
+  "/:user_id",
+  loginMiddleware,
+  async (req: Request, res: Response) => {
+    const { username, fullname, password, image_path } = req.body;
+    const { user_id } = req.params;
+    const user_service = new UserService();
+    const response = await user_service.editUser(
+      parseInt(user_id),
+      username,
+      fullname,
+      password,
+      image_path
+    );
+    if (response === Error.EDIT_FAILED) {
+      return res.json({
+        status: 400,
+        message: Error.EDIT_FAILED,
+      });
+    }
     return res.json({
-      status: 400,
-      message: Error.EDIT_FAILED,
+      status: 200,
+      data: response,
     });
   }
-  return res.json({
-    status: 200,
-    data: response,
-  });
-});
+);
 
-userRouter.delete("/:user_id",adminMiddleware ,async (req: Request, res: Response) => {
-  const { user_id } = req.params;
-  const user_service = new UserService();
-  const response = await user_service.deleteUser(parseInt(user_id));
-  if (response === Error.DELETE_FAILED) {
+userRouter.delete(
+  "/:user_id",
+  adminMiddleware,
+  async (req: Request, res: Response) => {
+    const { user_id } = req.params;
+    const user_service = new UserService();
+    const response = await user_service.deleteUser(parseInt(user_id));
+    if (response === Error.DELETE_FAILED) {
+      return res.json({
+        status: 404,
+        message: Error.DELETE_FAILED,
+      });
+    }
     return res.json({
-      status: 404,
-      message: Error.DELETE_FAILED,
+      status: 200,
+      message: response,
     });
   }
-  return res.json({
-    status: 200,
-    message: response,
-  });
-});
+);
 
-userRouter.get("/search", adminMiddleware,async (req: Request, res: Response) => {
-  const { username, page } = req.query;
-  const user_service = new UserService();
-  const username_query = username ? username.toString() : "";
-  const page_number = page ? parseInt(page.toString(), 10) : 1;
-  const response = await user_service.searchUserPagination(
-    username_query,
-    page_number
-  );
-  if (response === Error.FETCH_FAILED) {
+userRouter.get(
+  "/search",
+  adminMiddleware,
+  async (req: Request, res: Response) => {
+    const { username, page } = req.query;
+    const user_service = new UserService();
+    const username_query = username ? username.toString() : "";
+    const page_number = page ? parseInt(page.toString(), 10) : 1;
+    const response = await user_service.searchUserPagination(
+      username_query,
+      page_number
+    );
+    if (response === Error.FETCH_FAILED) {
+      return res.json({
+        status: 400,
+        message: Error.FETCH_FAILED,
+      });
+    }
+    if (response.length === 0) {
+      return res.json({
+        status: 400,
+        message: Error.USER_NOT_FOUND,
+      });
+    }
     return res.json({
-      status: 400,
-      message: Error.FETCH_FAILED,
+      status: 200,
+      data: response,
     });
   }
-  if (response.length === 0) {
-    return res.json({
-      status: 400,
-      message: Error.USER_NOT_FOUND,
-    });
-  }
-  return res.json({
-    status: 200,
-    data: response,
-  });
-});
+);
 
 userRouter.post("/username", async (req: Request, res: Response) => {
   const { username } = req.body;
@@ -126,7 +134,14 @@ userRouter.post("/username", async (req: Request, res: Response) => {
   });
 });
 
-userRouter.get("/:user_id", loginMiddleware,async (req: Request, res: Response) => {
+userRouter.get("/isAdmin", async (req: Request, res: Response) => {
+  const payload = new Payload().getCookie(req);
+  console.log(payload)
+  const isAdmin = payload.isAdmin;
+  return res.json(new SuccessResponse(isAdmin));
+});
+
+userRouter.get("/:user_id", async (req: Request, res: Response) => {
   const { user_id } = req.params;
   const user_service = new UserService();
   const response = await user_service.getUser(parseInt(user_id));
