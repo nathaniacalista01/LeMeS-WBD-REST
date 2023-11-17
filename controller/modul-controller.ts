9a75ab79ee8f6bd84aa7968b313e7ad33a842152
nathaniacalista01 <nathania.calista01@gmail.com>
2023-11-17 10:33:36 +0700

    fix : middleware for modul

--- controller/modul-controller.ts
+++ controller/modul-controller.ts
@@ -5,10 +5,13 @@ import { Payload } from "../utils/payload";
 import { Error } from "../types/type";
 import { FailedResponse, SuccessResponse } from "../utils/template";
 import { validateTeacher } from "../utils/validate-teacher";
+import { adminMiddleware } from "../middleware/admin-middleware";
+import { checkAPI } from "../middleware/api-middleware";
+import { CourseService } from "../service/course-service";
 
 export const modulRouter = express.Router();
 
-modulRouter.get("/", async (req: Request, res: Response) => {
+modulRouter.get("/", adminMiddleware, async (req: Request, res: Response) => {
   // Hanya bisa diakses oleh admin
   const payload = new Payload().getCookie(req);
   if (!payload || !payload.isAdmin) {
@@ -37,7 +40,7 @@ modulRouter.get("/", async (req: Request, res: Response) => {
   });
 });
 
-modulRouter.post("/", async (req: Request, res: Response) => {
+modulRouter.post("/", loginMiddleware, async (req: Request, res: Response) => {
   // Hanya bisa diakses oleh guru yang mengajar pada course itu
   const { title, description, course_id } = req.body;
   const modul_service = new ModulService();
@@ -66,63 +69,78 @@ modulRouter.post("/", async (req: Request, res: Response) => {
   });
 });
 
-modulRouter.put("/:modul_id", async (req: Request, res: Response) => {
-  const { modul_id } = req.params;
-  const { title, description } = req.body;
-  const modul_service = new ModulService();
-  const payload = new Payload().getCookie(req);
-  const teacher_id = await modul_service.getTeacherByModulId(
-    parseInt(modul_id)
-  );
-  const isValid = validateTeacher(payload, teacher_id);
-  if (isValid) {
-    return res.json(isValid);
-  }
-  const response = await modul_service.editModul(
-    parseInt(modul_id),
-    title,
-    description
-  );
-  if (response === Error.EDIT_FAILED) {
+modulRouter.put(
+  "/:modul_id",
+  loginMiddleware,
+  async (req: Request, res: Response) => {
+    // Update modul hanya dapat dilakuakn oleh teacher
+    const { modul_id } = req.params;
+    const { title, description } = req.body;
+    const modul_service = new ModulService();
+    const payload = new Payload().getCookie(req);
+    const teacher_id = await modul_service.getTeacherByModulId(
+      parseInt(modul_id)
+    );
+    const isValid = validateTeacher(payload, teacher_id);
+    if (isValid) {
+      return res.json(isValid);
+    }
+    const response = await modul_service.editModul(
+      parseInt(modul_id),
+      title,
+      description
+    );
+    if (response === Error.EDIT_FAILED) {
+      return res.json({
+        status: 500,
+        message: Error.EDIT_FAILED,
+      });
+    }
     return res.json({
-      status: 500,
-      message: Error.EDIT_FAILED,
+      status: 200,
+      data: response,
     });
   }
-  return res.json({
-    status: 200,
-    data: response,
-  });
-});
-modulRouter.delete("/:modul_id", async (req: Request, res: Response) => {
+);
+modulRouter.delete(
+  "/:modul_id",
+  loginMiddleware,
+  async (req: Request, res: Response) => {
+    // Modul hanya dapat di delete oleh teacher yang mengajar di sana
+    const { modul_id } = req.params;
+    const modul_service = new ModulService();
+    const teacher_id = await modul_service.getTeacherByModulId(
+      parseInt(modul_id)
+    );
+    const payload = new Payload().getCookie(req);
+    const notValid = validateTeacher(payload, teacher_id);
+    if (notValid) {
+      return res.json(notValid);
+    }
+    const response = await modul_service.deleteModul(parseInt(modul_id));
+    if (response === Error.DELETE_FAILED) {
+      res.json(new FailedResponse(500, Error.INTERNAL_ERROR));
+    }
+    res.json(new SuccessResponse(response));
+  }
+);
+
+modulRouter.get("/:modul_id", checkAPI, async (req: Request, res: Response) => {
+  // Mengakses modul bisa dari PHP dan bisa dari express
   const { modul_id } = req.params;
   const modul_service = new ModulService();
   const teacher_id = await modul_service.getTeacherByModulId(
     parseInt(modul_id)
   );
-  const payload = new Payload().getCookie(req);
-  const notValid = validateTeacher(payload, teacher_id);
-  if (notValid) {
-    return res.json(notValid);
+  // Validasi tambahan kalau yang akses modul beneran teacher yang ngajar
+  if (req.headers["X-API-KEY"] === process.env.REACT_API_KEY) {
+    const payload = new Payload().getCookie(req);
+    const notValid = validateTeacher(payload, teacher_id);
+    if (notValid) {
+      return res.json(notValid);
+    }
   }
-  const response = await modul_service.deleteModul(parseInt(modul_id));
-  if (response === Error.DELETE_FAILED) {
-    res.json(new FailedResponse(500, Error.INTERNAL_ERROR));
-  }
-  res.json(new SuccessResponse(response));
-});
-
-modulRouter.get("/:modul_id", async (req: Request, res: Response) => {
-  const { modul_id } = req.params;
-  const modul_service = new ModulService();
-  // const teacher_id = await modul_service.getTeacherByModulId(
-  //   parseInt(modul_id)
-  // );
-  // const payload = new Payload().getCookie(req);
-  // const notValid = validateTeacher(payload, teacher_id);
-  // if (notValid) {
-  //   return res.json(notValid);
-  // }
+  // Fetch modul
   const response = await modul_service.getModul(parseInt(modul_id));
   if (response === Error.FETCH_FAILED) {
     return res.json(new FailedResponse(500, Error.FETCH_FAILED));
@@ -133,15 +151,23 @@ modulRouter.get("/:modul_id", async (req: Request, res: Response) => {
   return res.json(new SuccessResponse(response));
 });
 
-modulRouter.get("/course/:course_id", async (req: Request, res: Response) => {
+modulRouter.get("/course/:course_id", checkAPI,async (req: Request, res: Response) => {
+  // Mendapatkan modul - modul dari id course yang ada bisa dari PHP dan express
   const { course_id } = req.params;
   const module_service = new ModulService();
+  const teacher = await module_service.getTeacherByCourseId(parseInt(course_id));
+  if(req.headers["x-api-key"] === "ReactApp"){
+    const payload = new Payload().getCookie(req);
+    if(payload.id !== teacher){
+      return res.json(new FailedResponse(404, Error.UNAUTHORZIED_ACTION));
+    }
+  }
   const moduls = await module_service.getModulsCourse(parseInt(course_id));
-  if(moduls === Error.FETCH_FAILED){
+  if (moduls === Error.FETCH_FAILED) {
     return res.json(new FailedResponse(500, Error.FETCH_FAILED));
   }
-  if(!moduls){
-    return res.json(new FailedResponse(404, Error.MDOULE_NOT_FOUND))
+  if (!moduls) {
+    return res.json(new FailedResponse(404, Error.MDOULE_NOT_FOUND));
   }
   return res.json(new SuccessResponse(moduls));
 });
